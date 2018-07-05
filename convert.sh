3e245bee6c3edfda88dac6da7db32b1b3eafbd7a
Oliver Lantwin <oliver.lantwin@cern.ch>
2018-07-05 23:49:00 +0200

    Use docker

--- convert.sh
+++ convert.sh
@@ -11,7 +11,7 @@ inotifywait -r -m "$DIR" -e create -e moved_to |
 	# OUTPUTFILE=$(basename $FILE .rawdata).root
 	OUTPUTFILE=$(basename $FILE .raw).root
 	LOGFILE=conversion_$(basename $FILE .raw).log
-	[[ $(basename $FILE) =~ ^SPILLDATA ]] && root -q -b "unpack_tdc.C(\"$path$FILE\", \"$OUTPUTFILE\", $RUN)" > conversion_$(basename $FILE .raw).log 2>&1 
+	[[ $(basename $FILE) =~ ^SPILLDATA ]] && sudo docker run -v $DIR:$DIR:Z olantwin/muon_flux_online:latest bin/bash -c "root -q -b \"unpack_tdc.C(\\\"$path$FILE\\\", \\\"$path$OUTPUTFILE\\\", $RUN)\" > $path$LOGFILE 2>&1"
 	OUTPUTPATH=/eos/experiment/ship/data/muflux/rawdata/$RUNDIR
 	xrdfs $EOSSHIP stat $OUTPUTPATH || xrdfs $EOSSHIP mkdir $OUTPUTPATH
 	xrdcp $OUTPUTFILE $EOSSHIP$OUTPUTPATH && rm $OUTPUTFILE && ./elog.py --text "Uploaded file $OUTPUTFILE of run $RUN; logfile: $HOSTNAME:$PWD/$LOGFILE" --subject "TEST: File upload run $RUN" --run $RUN
