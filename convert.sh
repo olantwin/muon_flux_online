0231e25d55664e78856221edb1dfc49d7add6b33
Oliver Lantwin <oliver.lantwin@cern.ch>
2018-07-06 01:11:29 +0200

    Update conversion script

--- convert.sh
+++ convert.sh
@@ -8,10 +8,9 @@ inotifywait -r -m "$DIR" -e create -e moved_to |
         echo "The file '$FILE' appeared in directory '$path' via '$action'"
 	RUNDIR=$(basename $path)
 	RUN=${RUNDIR:4}
-	# OUTPUTFILE=$(basename $FILE .rawdata).root
-	OUTPUTFILE=$(basename $FILE .raw).root
-	LOGFILE=conversion_$(basename $FILE .raw).log
-	[[ $(basename $FILE) =~ ^SPILLDATA ]] && sudo docker run -v $DIR:$DIR:Z olantwin/muon_flux_online:latest bin/bash -c "root -q -b \"unpack_tdc.C(\\\"$path$FILE\\\", \\\"$path$OUTPUTFILE\\\", $RUN)\" > $path$LOGFILE 2>&1"
+	OUTPUTFILE=$(basename $FILE .rawdata).root
+	LOGFILE=conversion_$(basename $FILE .rawdata).log
+	[[ $(basename $FILE) =~ ^SPILLDATA ]] && docker run -v $DIR:$DIR:Z -v $PWD:/workdir:Z olantwin/muon_flux_online:v1 bash -c "cd muon_flux_online; alienv setenv FairShip/latest -c root -q -b \"unpack_tdc.C(\\\"$path$FILE\\\", \\\"/workdir/$OUTPUTFILE\\\", $RUN)\" > /workdir/$LOGFILE 2>&1"
 	OUTPUTPATH=/eos/experiment/ship/data/muflux/rawdata/$RUNDIR
 	xrdfs $EOSSHIP stat $OUTPUTPATH || xrdfs $EOSSHIP mkdir $OUTPUTPATH
 	xrdcp $OUTPUTFILE $EOSSHIP$OUTPUTPATH && rm $OUTPUTFILE && ./elog.py --text "Uploaded file $OUTPUTFILE of run $RUN; logfile: $HOSTNAME:$PWD/$LOGFILE" --subject "TEST: File upload run $RUN" --run $RUN
