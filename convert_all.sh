066a02e9bea6082a6d88f30649615d97965f0651
Oliver Lantwin <oliver.lantwin@cern.ch>
2018-07-07 00:03:34 +0200

    Update convert_all.sh

--- convert_all.sh
+++ convert_all.sh
@@ -3,7 +3,7 @@
 set -x
 
 DIR=$1
-TAG=DTv1
+TAG=DTv2
 for FILE in $DIR/*.rawdata; do
     [[ $(basename $FILE) =~ ^SPILLDATA ]] && continue
     OUTPUTFILE=$(basename $FILE .rawdata).root
@@ -12,5 +12,7 @@ for FILE in $DIR/*.rawdata; do
     OUTPUTPATH=/eos/experiment/ship/data/muflux/rawdata/$DIR
     xrdfs $EOSSHIP stat $OUTPUTPATH || xrdfs $EOSSHIP mkdir $OUTPUTPATH
     xrdfs $EOSSHIP stat $OUTPUTPATH$OUTPUTFILE || docker run -v $DIR:$DIR:Z -v $PWD:/workdir:Z olantwin/muon_flux_online:$TAG bash -c "cd muon_flux_online; alienv setenv FairShip/latest -c root -q -b \"unpack_tdc.C(\\\"$DIR$FILE\\\", \\\"/workdir/$OUTPUTFILE\\\", $RUN)\" > /workdir/$LOGFILE 2>&1"
-    xrdcp $OUTPUTFILE $EOSSHIP$OUTPUTDIR && rm $OUTPUTFILE && ./elog.py --text "Uploaded file $OUTPUTFILE of run $RUN; logfile: $HOSTNAME:$PWD/$LOGFILE" --subject "File upload run $RUN" --run $RUN
+    xrdcp $OUTPUTFILE $EOSSHIP$OUTPUTDIR && rm $OUTPUTFILE
 done
+./elog.py --text "Converted all unconverted files of run $RUN using tag $TAG" --subject "Converted mising filed for run $RUN" --run $RUN
+
