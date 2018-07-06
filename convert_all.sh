345765905ecfa909a25bbec3d63cdf76af841f2a
Oliver Lantwin <oliver.lantwin@cern.ch>
2018-07-07 00:43:57 +0200

    Remove write-protected file

--- convert_all.sh
+++ convert_all.sh
@@ -13,7 +13,7 @@ for FILE in $DIR/*.rawdata; do
     OUTPUTPATH=/eos/experiment/ship/data/muflux/rawdata/$RUNDIR
     xrdfs $EOSSHIP stat $OUTPUTPATH || xrdfs $EOSSHIP mkdir $OUTPUTPATH
     xrdfs $EOSSHIP stat $OUTPUTPATH/$OUTPUTFILE || docker run -v $DIR:$DIR:Z -v $PWD:/workdir:Z olantwin/muon_flux_online:$TAG bash -c "cd muon_flux_online; alienv setenv FairShip/latest -c root -q -b \"unpack_tdc.C(\\\"$DIR$FILE\\\", \\\"/workdir/$OUTPUTFILE\\\", $RUN)\" > /workdir/$LOGFILE 2>&1"
-    xrdcp $OUTPUTFILE $EOSSHIP$OUTPUTPATH && rm $OUTPUTFILE
+    xrdcp $OUTPUTFILE $EOSSHIP$OUTPUTPATH && rm -f $OUTPUTFILE
 done
 ./elog.py --text "Converted all unconverted files of run $RUN using tag $TAG" --subject "Converted mising filed for run $RUN" --run $RUN
 
