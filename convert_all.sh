7e6181305ab5c134a5ca6019af462ed41083d72b
Oliver Lantwin <oliver.lantwin@cern.ch>
2018-07-07 00:21:55 +0200

    Fix directory assumptions

--- convert_all.sh
+++ convert_all.sh
@@ -8,8 +8,9 @@ for FILE in $DIR/*.rawdata; do
     [[ $(basename $FILE) =~ ^SPILLDATA ]] || continue
     OUTPUTFILE=$(basename $FILE .rawdata).root
     LOGFILE=conversion_$(basename $FILE .rawdata).log
-    RUN=${DIR:4}
-    OUTPUTPATH=/eos/experiment/ship/data/muflux/rawdata/$DIR
+    RUNDIR=$(basename $DIR)
+    RUN=${RUNDIR:4}
+    OUTPUTPATH=/eos/experiment/ship/data/muflux/rawdata/$RUNDIR
     xrdfs $EOSSHIP stat $OUTPUTPATH || xrdfs $EOSSHIP mkdir $OUTPUTPATH
     xrdfs $EOSSHIP stat $OUTPUTPATH$OUTPUTFILE || docker run -v $DIR:$DIR:Z -v $PWD:/workdir:Z olantwin/muon_flux_online:$TAG bash -c "cd muon_flux_online; alienv setenv FairShip/latest -c root -q -b \"unpack_tdc.C(\\\"$DIR$FILE\\\", \\\"/workdir/$OUTPUTFILE\\\", $RUN)\" > /workdir/$LOGFILE 2>&1"
     xrdcp $OUTPUTFILE $EOSSHIP$OUTPUTDIR && rm $OUTPUTFILE
