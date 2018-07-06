99961753aefe5a0b35b62cdf8a49b098bcf2886f
Oliver Lantwin <oliver.lantwin@cern.ch>
2018-07-06 03:31:20 +0200

    Update batch conversion

--- convert_all.sh
+++ convert_all.sh
@@ -1,8 +1,16 @@
 #!/bin/bash
 
+set -x
+
 DIR=$1
-for FILE in $(ls $DIR/*.rawdata); do
+TAG=DTv1
+for FILE in $DIR/*.rawdata; do
+    [[ $(basename $FILE) =~ ^SPILLDATA ]] && continue
     OUTPUTFILE=$(basename $FILE .rawdata).root
-    root -q -b "unpack_tdc.C(\"$FILE\", \"$OUTPUTFILE\")" > conversion.log 2>&1 
-    xrdcp $OUTPUTFILE $EOSSHIP/eos/experiment/ship/data/muflux/rawdata/ && rm $OUTPUTFILE
+    LOGFILE=conversion_$(basename $FILE .rawdata).log
+    RUN=${DIR:4}
+    OUTPUTPATH=/eos/experiment/ship/data/muflux/rawdata/$DIR
+    xrdfs $EOSSHIP stat $OUTPUTPATH || xrdfs $EOSSHIP mkdir $OUTPUTPATH
+    xrdfs $EOSSHIP stat $OUTPUTPATH$OUTPUTFILE || docker run -v $DIR:$DIR:Z -v $PWD:/workdir:Z olantwin/muon_flux_online:$TAG bash -c "cd muon_flux_online; alienv setenv FairShip/latest -c root -q -b \"unpack_tdc.C(\\\"$DIR$FILE\\\", \\\"/workdir/$OUTPUTFILE\\\", $RUN)\" > /workdir/$LOGFILE 2>&1"
+    xrdcp $OUTPUTFILE $EOSSHIP$OUTPUTDIR && rm $OUTPUTFILE && ./elog.py --text "Uploaded file $OUTPUTFILE of run $RUN; logfile: $HOSTNAME:$PWD/$LOGFILE" --subject "File upload run $RUN" --run $RUN
 done
