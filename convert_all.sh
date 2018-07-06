271aa692224276987a3d862e2bdf78084278fcbd
Oliver Lantwin <oliver.lantwin@cern.ch>
2018-07-07 00:25:08 +0200

    Final fix?

--- convert_all.sh
+++ convert_all.sh
@@ -12,8 +12,8 @@ for FILE in $DIR/*.rawdata; do
     RUN=${RUNDIR:4}
     OUTPUTPATH=/eos/experiment/ship/data/muflux/rawdata/$RUNDIR
     xrdfs $EOSSHIP stat $OUTPUTPATH || xrdfs $EOSSHIP mkdir $OUTPUTPATH
-    xrdfs $EOSSHIP stat $OUTPUTPATH$OUTPUTFILE || docker run -v $DIR:$DIR:Z -v $PWD:/workdir:Z olantwin/muon_flux_online:$TAG bash -c "cd muon_flux_online; alienv setenv FairShip/latest -c root -q -b \"unpack_tdc.C(\\\"$DIR$FILE\\\", \\\"/workdir/$OUTPUTFILE\\\", $RUN)\" > /workdir/$LOGFILE 2>&1"
-    xrdcp $OUTPUTFILE $EOSSHIP$OUTPUTDIR && rm $OUTPUTFILE
+    xrdfs $EOSSHIP stat $OUTPUTPATH/$OUTPUTFILE || docker run -v $DIR:$DIR:Z -v $PWD:/workdir:Z olantwin/muon_flux_online:$TAG bash -c "cd muon_flux_online; alienv setenv FairShip/latest -c root -q -b \"unpack_tdc.C(\\\"$DIR$FILE\\\", \\\"/workdir/$OUTPUTFILE\\\", $RUN)\" > /workdir/$LOGFILE 2>&1"
+    xrdcp $OUTPUTFILE $EOSSHIP$OUTPUTPATH && rm $OUTPUTFILE
 done
 ./elog.py --text "Converted all unconverted files of run $RUN using tag $TAG" --subject "Converted mising filed for run $RUN" --run $RUN
 
