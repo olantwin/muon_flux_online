0d51fa8073bba64d09b0058609a85af79116b9d4
Oliver Lantwin <oliver.lantwin@cern.ch>
2018-07-07 10:46:17 +0200

    Finish online.sh

--- convert.sh
+++ convert.sh
@@ -7,11 +7,11 @@ TAG=DTv3
 inotifywait -r -m "$DIR" -e create -e moved_to |
     while read path action FILE; do
         echo "The file '$FILE' appeared in directory '$path' via '$action'"
-	[[ $(basename $FILE) =~ ^RUN_ ]] && RUNDIR=$(basename $FILE) && RUN=${RUNDIR:4} && ./elog.py --text "New run $RUN being converted using tag $TAG" --subject "Start conversion run $RUN" --run $RUN && continue
+	[[ $(basename $FILE) =~ ^RUN_ ]] && RUNDIR=$(basename $FILE) && RUN=${RUNDIR:4} && RUN=$((10#$RUN)) && ./elog.py --text "New run $RUN being converted using tag $TAG" --subject "Start conversion run $RUN" --run $RUN && continue
 	RUNDIR=$(basename $path)
 	RUN=${RUNDIR:4}
 	[[ $RUN == 0000000000 ]] && continue
-    RUN=$((10#$RUN))
+	RUN=$((10#$RUN))
 	OUTPUTFILE=$(basename $FILE .rawdata).root
 	LOGFILE=conversion_$(basename $FILE .rawdata).log
 	[[ $(basename $FILE) =~ ^SPILLDATA ]] && docker run -v $DIR:$DIR:Z -v $PWD:/workdir:Z olantwin/muon_flux_online:$TAG bash -c "cd muon_flux_online; alienv setenv FairShip/latest -c root -q -b \"unpack_tdc.C(\\\"$path$FILE\\\", \\\"/workdir/$OUTPUTFILE\\\", $RUN)\" > /workdir/$LOGFILE 2>&1"
--- online.sh
+++ online.sh
@@ -6,12 +6,12 @@ DIR=/data/SHiP_testBeam/Data
 inotifywait -r -m "$DIR" -e create -e moved_to |
     while read path action FILE; do
         echo "The file '$FILE' appeared in directory '$path' via '$action'"
+	[[ $(basename $FILE) =~ ^RUN_ ]] && RUNDIR=$(basename $FILE) && RUN=${RUNDIR:4} && RUN=$((10#$RUN)) && ./elog.py --text "New run $RUN being uploaded" --subject "Start conversion run $RUN" --run $RUN && continue
 	RUNDIR=$(basename $path)
 	RUN=${RUNDIR:4}
-	# TODO what other files will there be apart from the spilldata?
+	RUN=$((10#$RUN))
 	OUTPUTPATH=/eos/experiment/ship/data/muflux/rawdata/$RUNDIR
 	xrdfs $EOSSHIP stat $OUTPUTPATH || xrdfs $EOSSHIP mkdir $OUTPUTPATH
-	xrdcp $FILE $EOSSHIP$OUTPUTPATH && rm $FILE && ./elog.py --text "Uploaded file $FILE of run $RUN" --subject "EB raw file upload run $RUN" --run $RUN
-	# TODO get lock for filename file
-	printf "%s\\n" "$EOSSHIP$OUTPUTPATH$(basename "$FILE")" >> files_to_convert.txt
+	xrdcp $FILE $EOSSHIP$OUTPUTPATH && rm $FILE
+	flock files_to_convert.lock printf "%s\\n" "$EOSSHIP$OUTPUTPATH$(basename "$FILE")" >> files_to_convert.txt
     done
