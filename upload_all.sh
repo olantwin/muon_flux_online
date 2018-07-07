2b6a17b3dbe66f0ec353fa0c8610a47d7a70d32c
Oliver Lantwin <oliver.lantwin@cern.ch>
2018-07-07 12:46:55 +0200

    Upload all script and upload online

--- upload_all.sh
+++ upload_all.sh
@@ -3,8 +3,8 @@
 set -x
 
 DIR=/data/SHiP_testBeam/Data
-inotifywait -r -m "$DIR" -e create -e moved_to |
-    while read path action FILE; do
+
+for FILE in $DIR/RUN_8000_2**/*; do
         echo "The file '$FILE' appeared in directory '$path' via '$action'"
 	[[ $(basename $FILE) =~ ^RUN_ ]] && RUNDIR=$(basename $FILE) && RUN=${RUNDIR:4} && RUN=$((10#$RUN)) && ./elog.py --text "New run $RUN being uploaded" --subject "Start conversion run $RUN" --run $RUN && continue
 	RUNDIR=$(basename $path)
@@ -12,6 +12,7 @@ inotifywait -r -m "$DIR" -e create -e moved_to |
 	RUN=$((10#$RUN))
 	OUTPUTPATH=/eos/experiment/ship/data/muflux/rawdata/$RUNDIR
 	xrdfs $EOSSHIP stat $OUTPUTPATH || xrdfs $EOSSHIP mkdir $OUTPUTPATH
+	xrdfs $EOSSHIP stat $OUTPUTPATH/$(basename $FILE) && echo "File $FILE already uploaded" && continue
 	xrdcp $FILE $EOSSHIP$OUTPUTPATH
 	flock files_to_convert.lock echo "$EOSSHIP$OUTPUTPATH$(basename "$FILE")" >> files_to_convert.txt
     done
