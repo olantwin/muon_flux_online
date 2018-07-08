4e9a320976544510b4b86e3a6f49b6f8639b0555
Oliver Lantwin <oliver.lantwin@cern.ch>
2018-07-08 09:22:01 +0200

    Add sleep, only add to files to convert on success

--- upload_all.sh
+++ upload_all.sh
@@ -13,6 +13,5 @@ for FILE in $DIR/RUN_8000_????/*; do
 	OUTPUTPATH=/eos/experiment/ship/data/muflux/rawdata/$RUNDIR
 	xrdfs $EOSSHIP stat $OUTPUTPATH || xrdfs $EOSSHIP mkdir $OUTPUTPATH
 	xrdfs $EOSSHIP stat $OUTPUTPATH/$(basename $FILE) && echo "File $FILE already uploaded" && continue
-	xrdcp $FILE $EOSSHIP$OUTPUTPATH
-	flock files_to_convert.lock echo "$EOSSHIP$OUTPUTPATH$(basename "$FILE")" >> files_to_convert.txt
+	xrdcp $FILE $EOSSHIP$OUTPUTPATH && flock files_to_convert.lock echo "$EOSSHIP$OUTPUTPATH$(basename "$FILE")" >> files_to_convert.txt
     done
