f7dfb591e50f5f52272d435c6299c86c5be26115
Oliver Lantwin <oliver.lantwin@cern.ch>
2018-07-16 17:02:14 +0200

    Fix upload_all.sh as well

--- upload_all.sh
+++ upload_all.sh
@@ -3,15 +3,17 @@
 set -x
 set -u
 
-DIR=/data/SHiP_testBeam/Data
+RUNDIR=$1
+DIR=/data/SHiP_testBeam/Data/"$RUNDIR"
 
-for FILE in $DIR/RUN_8000_????/*; do
-	[[ $(basename $FILE) =~ ^RUN_8000_.{4}$ ]] && RUNDIR=$(basename $FILE) && RUN=${RUNDIR:9} && RUN=$((10#$RUN)) && continue
-	RUNDIR=$(basename $(dirname $FILE))
+for FILE in $DIR/*; do
+	[[ ! $FILE == *.raw ]] && continue
+	[[ $(basename "$FILE") =~ ^RUN_8000_.{4}$ ]] && RUNDIR=$(basename "$FILE") && RUN=${RUNDIR:9} && RUN=$((10#$RUN)) && continue
+	RUNDIR=$(basename "$(dirname "$FILE")")
 	RUN=${RUNDIR:9}
 	RUN=$((10#$RUN))
 	OUTPUTPATH=/eos/experiment/ship/data/muflux/rawdata/$RUNDIR
-	xrdfs $EOSSHIP stat $OUTPUTPATH || xrdfs $EOSSHIP mkdir $OUTPUTPATH
-	xrdfs $EOSSHIP stat $OUTPUTPATH/$(basename $FILE) && echo "File $FILE already uploaded" && continue
-	xrdcp $FILE $EOSSHIP$OUTPUTPATH && flock files_to_convert.lock echo "$EOSSHIP$OUTPUTPATH$(basename "$FILE")" >> files_to_convert.txt
+	xrdfs "$EOSSHIP" stat "$OUTPUTPATH" || xrdfs "$EOSSHIP" mkdir "$OUTPUTPATH"
+	xrdfs "$EOSSHIP" stat "$OUTPUTPATH"/"$(basename "$FILE")" && echo "File $FILE already uploaded" && continue
+	xrdcp "$FILE" "$EOSSHIP""$OUTPUTPATH" && flock files_to_convert.lock echo "$EOSSHIP$OUTPUTPATH$(basename "$FILE")" >> files_to_convert.txt
     done
