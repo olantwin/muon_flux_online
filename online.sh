e9f42aff787c9e558a05f644ef351140a5e3212d
Oliver Lantwin <oliver.lantwin@cern.ch>
2018-07-16 16:55:51 +0200

    Update online.sh for new Evb

--- online.sh
+++ online.sh
@@ -6,6 +6,7 @@ DIR=/data/SHiP_testBeam/Data
 inotifywait -r -m "$DIR" -e create -e moved_to |
     while read -r path action FILE; do
         echo "The file '$FILE' appeared in directory '$path' via '$action'"
+	[[ ! $FILE == *.raw ]] && continue
 	[[ $(basename "$FILE") =~ ^RUN_8000_.{4}$ ]] && RUNDIR=$(basename "$FILE") && RUN=${RUNDIR:9} && RUN=$((10#$RUN)) && ./elog.py --text "New run $RUN being uploaded" --subject "Start conversion run $RUN" --run $RUN && continue
 	RUNDIR=$(basename "$path")
 	RUN=${RUNDIR:9}
@@ -13,7 +14,6 @@ inotifywait -r -m "$DIR" -e create -e moved_to |
 	RUN=$((10#$RUN))
 	OUTPUTPATH=/eos/experiment/ship/data/muflux/rawdata/$RUNDIR
 	xrdfs "$EOSSHIP" stat "$OUTPUTPATH" || xrdfs "$EOSSHIP" mkdir "$OUTPUTPATH"
-	sleep 3s
 	xrdcp "$path""$FILE" "$EOSSHIP""$OUTPUTPATH"
 	if [[ $(basename "$FILE") =~ ^SPILLDATA ]]; then
 	    OUTPUTFILE=$(basename "$FILE" .raw).root
