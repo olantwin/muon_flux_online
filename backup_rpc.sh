a93d84b1fc6938ff7fbaeabe7d50c5e6b26a60d2
Oliver Lantwin <oliver.lantwin@cern.ch>
2018-07-18 17:37:02 +0200

    Add sleep for RPC backup

--- backup_rpc.sh
+++ backup_rpc.sh
@@ -14,6 +14,7 @@ inotifywait -r -m "$DIR" -e create -e moved_to |
 	RUN=$((10#$RUN))
 	RUNDIR=RUN_0B00_$(printf "%04d" $RUN)
 	OUTPUTPATH=/eos/experiment/ship/data/muflux/rawdata/$RUNDIR
+	sleep 6s
 	xrdfs "$EOSSHIP" stat "$OUTPUTPATH" || xrdfs "$EOSSHIP" mkdir "$OUTPUTPATH"
 	xrdfs "$EOSSHIP" stat "$OUTPUTPATH"/"$(basename "$FILE")" && echo "File $FILE already uploaded" && continue
 	xrdcp "$path""$FILE" "$EOSSHIP""$OUTPUTPATH"
