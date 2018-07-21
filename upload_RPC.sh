ac304a05b48b21219c1de037c90ddce7b62f7309
Oliver Lantwin <oliver.lantwin@cern.ch>
2018-07-21 09:36:43 +0200

    updated RPC script

--- upload_RPC.sh
+++ upload_RPC.sh
@@ -1,18 +1,31 @@
 #!/bin/bash
 
-set -x
+in_list() {
+    local search="$1"
+    shift
+    local list=("$@")
+    for file in ${list[@]}; do
+        [[ $file == $search ]] && return 0
+    done
+    return 1
+}
+
 set -u
 
 DIR=$1
 EOSSHIP=root://eospublic.cern.ch/
 
+RUNDIR=$(basename "$DIR")
+RUN=${RUNDIR:3}
+RUN=$((10#$RUN))
+RUNDIR=RUN_0B00_$(printf "%04d" $RUN)
+OUTPUTPATH=/eos/experiment/ship/data/muflux/rawdata/$RUNDIR
+xrdfs "$EOSSHIP" stat "$OUTPUTPATH" || xrdfs "$EOSSHIP" mkdir "$OUTPUTPATH"
+EOSFILES=$(xrdfs "$EOSSHIP" ls "$OUTPUTPATH")
 for FILE in $DIR/*.dat; do
-	RUNDIR=$(basename "$(dirname "$FILE")")
-	RUN=${RUNDIR:3}
-	RUN=$((10#$RUN))
-	RUNDIR=RUN_0B00_$(printf "%04d" $RUN)
-	OUTPUTPATH=/eos/experiment/ship/data/muflux/rawdata/$RUNDIR
-	xrdfs "$EOSSHIP" stat "$OUTPUTPATH" || xrdfs "$EOSSHIP" mkdir "$OUTPUTPATH"
-	xrdfs "$EOSSHIP" stat "$OUTPUTPATH"/"$(basename "$FILE")" && echo "File $FILE already uploaded" && continue
+    if ! in_list "$OUTPUTPATH"/"$(basename "$FILE")" "$EOSFILES"; then
 	xrdcp "$FILE" "$EOSSHIP""$OUTPUTPATH"
-    done
+    else
+	echo "File $FILE already uploaded"
+    fi
+done
