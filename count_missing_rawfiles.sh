bff30eb46aeeef924ee0ad95db0906e60c569835
Oliver Lantwin <oliver.lantwin@cern.ch>
2018-07-27 14:02:55 +0200

    Add script to check for missing files

---
+++ count_missing_rawfiles.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+set -u
+
+LOCAL=$1
+REMOTE=/eos/experiment/ship/data/muflux/rawdata
+
+in_list() {
+    local search="$1"
+    shift
+    local list=("$@")
+    for file in ${list[@]}; do
+        [[ $file == $search ]] && return 0
+    done
+    return 1
+}
+
+count=0
+for RUN in $LOCAL; do
+    [[ $(basename "$RUN") =~ ^RUN_8000_2.{3}$ ]] || continue
+    SPILLS=$RUN/*.raw*
+    OUTPUTPATH=/eos/experiment/ship/data/muflux/rawdata/"$RUN"
+    REMOTE_FILES=$(xrdfs "$EOSSHIP" ls "$OUTPUTPATH")
+    for SPILL in $SPILLS; do
+	if ! in_list "$SPILL" "$REMOTE_FILES"; then
+	    echo "$SPILL" >> files_to_upload.txt
+	    (( count ++ ))
+	fi
+    done
+done
+echo $count
